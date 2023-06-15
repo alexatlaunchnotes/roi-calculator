1ad2fcea0da61a989643df64b99b61665d7301b4
Alex Villacres <alexvillacres@Alexs-Laptop.lan>
2023-06-15 10:48:37 -1000

    Added ajax file for Webflow Form handling

--- src/index.ts
+++ src/index.ts
@@ -20,96 +20,97 @@ const formWrapper = document.getElementById("roi-form") as HTMLDivElement;
 const results = document.getElementById("roi-results") as HTMLDivElement;
 
 const init = () => {
-// Event listeners
-nextButton.addEventListener('click', handleNextButtonClick)
-nextButton.addEventListener('touchstart', handleNextButtonClick)
-submitButton.addEventListener('click', handleSubmitButtonClick)
-submitButton.addEventListener('touchstart', handleSubmitButtonClick)
-prevButton.addEventListener('click', handlePreviousButtonClick)
-prevButton.addEventListener('touchstart', handlePreviousButtonClick)
+    
+    // Event listeners
+    nextButton.addEventListener('click', handleNextButtonClick)
+    nextButton.addEventListener('touchstart', handleNextButtonClick)
+    submitButton.addEventListener('click', handleSubmitButtonClick)
+    submitButton.addEventListener('touchstart', handleSubmitButtonClick)
+    prevButton.addEventListener('click', handlePreviousButtonClick)
+    prevButton.addEventListener('touchstart', handlePreviousButtonClick)
 
-function handleNextButtonClick(event: Event) {
-    event.preventDefault()
-    count += 1;
-    console.log(count)
+    function handleNextButtonClick(event: Event) {
+        event.preventDefault()
+        count += 1;
+        console.log(count)
 
-    updateNavigationButtons()
+        updateNavigationButtons()
 
-    // Get the form inputs
-    const salaryInput = document.getElementById("annual-salary") as HTMLInputElement
-    const employeesInput = document.getElementById("number-of-employees") as HTMLInputElement
-    const timeInput = document.getElementById("time-spent") as HTMLInputElement
+        // Get the form inputs
+        const salaryInput = document.getElementById("annual-salary") as HTMLInputElement
+        const employeesInput = document.getElementById("number-of-employees") as HTMLInputElement
+        const timeInput = document.getElementById("time-spent") as HTMLInputElement
 
-    // Update the variable values
-    annualSalary = parseFloat(salaryInput.value);
-    numberOfEmployees = parseFloat(employeesInput.value);
+        // Update the variable values
+        annualSalary = parseFloat(salaryInput.value);
+        numberOfEmployees = parseFloat(employeesInput.value);
+        }
     }
-}
 
-function handleSubmitButtonClick(event: Event) {
-    event.preventDefault()
+    function handleSubmitButtonClick(event: Event) {
+        event.preventDefault()
 
-    const timeInput = document.getElementById("time-spent") as HTMLInputElement
-    timeSpentOnProductChanges = parseFloat(timeInput.value);
+        const timeInput = document.getElementById("time-spent") as HTMLInputElement
+        timeSpentOnProductChanges = parseFloat(timeInput.value);
 
-    // Calculate the values
-    calculateMoneySpentAnnually();
-    calculateProductionTime();
-    caclulateProductChangeCost();
-    calculateMoneySaved();
-    calculateDaysSaved();
+        // Calculate the values
+        calculateMoneySpentAnnually();
+        calculateProductionTime();
+        caclulateProductChangeCost();
+        calculateMoneySaved();
+        calculateDaysSaved();
 
-    updateResults()
-}
+        updateResults()
+    }
 
-function handlePreviousButtonClick(event: Event) {
-    event.preventDefault()
-    count -= 1
-    console.log(count)
-    updateNavigationButtons()
-}
+    function handlePreviousButtonClick(event: Event) {
+        event.preventDefault()
+        count -= 1
+        console.log(count)
+        updateNavigationButtons()
+    }
 
-function updateResults() {
-    const productChangeCostElement = document.getElementById("product-change-cost") as HTMLElement;
-    const moneySavedElement = document.getElementById("money-saved") as HTMLElement;
-    const daysSavedElement = document.getElementById("days-saved") as HTMLElement;
+    function updateResults() {
+        const productChangeCostElement = document.getElementById("product-change-cost") as HTMLElement;
+        const moneySavedElement = document.getElementById("money-saved") as HTMLElement;
+        const daysSavedElement = document.getElementById("days-saved") as HTMLElement;
 
-    productChangeCostElement.textContent = productChangeCost?.toLocaleString() || "";
-    moneySavedElement.textContent = moneySaved?.toLocaleString() || "";
-    daysSavedElement.textContent = daysSaved?.toLocaleString() || "";
-}
+        productChangeCostElement.textContent = productChangeCost?.toLocaleString() || "";
+        moneySavedElement.textContent = moneySaved?.toLocaleString() || "";
+        daysSavedElement.textContent = daysSaved?.toLocaleString() || "";
+    }
 
-function calculateMoneySpentAnnually() {
-    if (annualSalary && numberOfEmployees) moneySpentAnnually = annualSalary * numberOfEmployees
-}
+    function calculateMoneySpentAnnually() {
+        if (annualSalary && numberOfEmployees) moneySpentAnnually = annualSalary * numberOfEmployees
+    }
 
-function calculateProductionTime() {
-    if (timeSpentOnProductChanges && numberOfEmployees) productionTime = timeSpentOnProductChanges / (5 * numberOfEmployees)
-}
+    function calculateProductionTime() {
+        if (timeSpentOnProductChanges && numberOfEmployees) productionTime = timeSpentOnProductChanges / (5 * numberOfEmployees)
+    }
 
-function caclulateProductChangeCost() {
-    if (moneySpentAnnually && productionTime) productChangeCost = moneySpentAnnually * productionTime
-}
+    function caclulateProductChangeCost() {
+        if (moneySpentAnnually && productionTime) productChangeCost = moneySpentAnnually * productionTime
+    }
 
-function calculateMoneySaved() {
-    if (productChangeCost) moneySaved = productChangeCost * 0.3
-}
+    function calculateMoneySaved() {
+        if (productChangeCost) moneySaved = productChangeCost * 0.3
+    }
 
-function calculateDaysSaved() {
-    if (timeSpentOnProductChanges && numberOfEmployees) daysSaved = (timeSpentOnProductChanges * numberOfEmployees) * 0.3
-}
+    function calculateDaysSaved() {
+        if (timeSpentOnProductChanges && numberOfEmployees) daysSaved = (timeSpentOnProductChanges * numberOfEmployees) * 0.3
+    }
 
-function updateNavigationButtons() {
-    if (count == 0) {
-        prevButton.style.display = "none";
-        nextButton.style.display = "flex";
-      } else if (count === 3) {
-        prevButton.style.display = "flex";
-        nextButton.style.display = "none";
-      } else {
-        prevButton.style.display = "flex";
-        nextButton.style.display = "flex";
-      }
+    function updateNavigationButtons() {
+        if (count == 0) {
+            prevButton.style.display = "none";
+            nextButton.style.display = "flex";
+        } else if (count === 3) {
+            prevButton.style.display = "flex";
+            nextButton.style.display = "none";
+        } else {
+            prevButton.style.display = "flex";
+            nextButton.style.display = "flex";
+        }
 }
 
 document.addEventListener("DOMContentLoaded", init)
